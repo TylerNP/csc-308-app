6c59aa491e613ecbb4d18277923a8448de710fbf
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-01-16 10:20:33 -0800

    allowed corrs on backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -31,6 +32,8 @@ const users = {
         }
     ]
 };
+
+app.use(cors());
 
 app.use(express.json())
 
