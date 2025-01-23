273e7d6c9874c4ad0a9860076aade0b662830d90
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-01-23 10:52:30 -0800

    updated test names and assertions

--- packages/jest-test/sample-functions.test.js
+++ packages/jest-test/sample-functions.test.js
@@ -1,109 +1,103 @@
 const myFunctions = require('./functions.js');
 
-test('Test div - with integer over integer', () => {
+test('Test div - with 10 / 2', () => {
     const target = 5;
     const result = myFunctions.div(10,2);
     expect(result).toBe(target);
 });
 
-test('Test div - with float over zero', () => {
+test('Test div - 2.975 / 0', () => {
     const target = Infinity;
     const result = myFunctions.div(2.975,0);
     expect(result).toBe(target);
 });
 
-test('Test div - with zero over float', () => {
+test('Test div - 0 / 2.975', () => {
     const target = 0;
     const result = myFunctions.div(0,2.975);
     expect(result).toBe(target);
 });
 
-test('Test div - with integer over float', () => {
-    const target = 2;
-    const result = myFunctions.div(5,2.5);
-    expect(result).toBe(target);
+test('Test div - with 10 / 3', () => {
+    const target = 3.33;
+    const result = myFunctions.div(10,3);
+    expect(result).toBeCloseTo(target);
 });
 
-test('Test div - with float over integer', () => {
-    const target = 0.5;
-    const result = myFunctions.div(2.5,5);
-    expect(result).toBe(target);
+test('Test div - with 3 / 10', () => {
+    const target = 0.3;
+    const result = myFunctions.div(3,10);
+    expect(result).toBeCloseTo(target);
 });
 
-test('Test div - with zero over zero', () => {
+test('Test div - with 0 / 0', () => {
     const target = NaN;
     const result = myFunctions.div(0,0);
     expect(result).toBe(target);
 });
 
-test('Test div - with negative zero over zero', () => {
+test('Test div - with -0 / 0', () => {
     const target = NaN;
     const result = myFunctions.div(-0,0);
     expect(result).toBe(target);
 });
 
-test('Test div - with positive integer over zero', () => {
+test('Test div - with 1 / 0', () => {
     const target = Infinity;
     const result = myFunctions.div(1,0);
     expect(result).toBe(target);
 });
 
-test('Test div - with negative integer over zero', () => {
+test('Test div - with -1 / 0', () => {
     const target = -Infinity;
     const result = myFunctions.div(-1,0);
     expect(result).toBe(target);
 });
 
-test('Test div - with zero over positive integer', () => {
+test('Test div - with 0 / 1', () => {
     const target = 0;
     const result = myFunctions.div(0,1);
     expect(result).toBe(target);
 });
 
-test('Test div - with zero over negative integer', () => {
+test('Test div - with 0 / -1', () => {
     const target = -0;
     const result = myFunctions.div(0,-1);
     expect(result).toBe(target);
 });
 
-test('Test div - with inifnity over zero', () => {
+test('Test div - with Infinity / 0', () => {
     const target = Infinity;
     const result = myFunctions.div(Infinity,0);
     expect(result).toBe(target);
 });
 
-test('Test div - with negative infinity over zero', () => {
+test('Test div - with -Infinity / 0', () => {
     const target = -Infinity;
     const result = myFunctions.div(-Infinity,0);
     expect(result).toBe(target);
 });
 
-test('Test div - with zero over inifnity', () => {
+test('Test div - with 0 / Inifnity', () => {
     const target = 0;
     const result = myFunctions.div(0,Infinity);
     expect(result).toBe(target);
 });
 
-test('Test div - with zero over negative infinity', () => {
+test('Test div - with 0 / -Infinity', () => {
     const target = -0;
     const result = myFunctions.div(0,-Infinity);
     expect(result).toBe(target);
 });
 
-test('Test div - with integer over inifnity', () => {
+test('Test div - with 1 / Inifnity', () => {
     const target = 0;
     const result = myFunctions.div(1,Infinity);
     expect(result).toBe(target);
 });
 
-test('Test div - with negative integer over infinity', () => {
+test('Test div - with -1 / Infinity', () => {
     const target = -0;
     const result = myFunctions.div(-1,Infinity);
-    expect(result).toBe(target);
-});
-
-test('Test containsNumbers - with zero over zero', () => {
-    const target = NaN;
-    const result = myFunctions.div(0,0);
     expect(result).toBe(target);
 });
